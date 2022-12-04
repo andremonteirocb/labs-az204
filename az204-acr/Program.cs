06524cb56d45789de8f63edb4290c5aa5f52f4f4
Andre Luis Soares Alves Monteiro <andre.monteiro@phidelis.com.br>
2022-12-04 00:57:58 -0300

    subindo o projeto de estudo para azure container registry

---
+++ az204-acr/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
