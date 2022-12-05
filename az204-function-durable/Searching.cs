250048bc4c40b297e40d0b248f222a2d64d7cc5b
Andre Luis Soares Alves Monteiro <andre.monteiro@phidelis.com.br>
2022-12-05 19:27:17 -0300

    adicionada a durable function para entidades;

--- az204-function-durable/Searching.cs
+++ az204-function-durable/Searching.cs
@@ -1,12 +1,12 @@
-using System;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 using Flurl.Http;
 
 namespace az204function.durable
@@ -16,16 +16,15 @@ namespace az204function.durable
         [FunctionName("Searching")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log,
-            [DurableClient] IDurableOrchestrationClient starter)
+            [DurableClient] IDurableOrchestrationClient starter,
+            ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string termo = req.Query["termo"];
+            var termo = req.Query["termo"];
             var instanceId = Guid.NewGuid().ToString();
 
-            string output = await starter.StartNewAsync("Pesquisador", instanceId, termo);
-
+            var output = await starter.StartNewAsync("Pesquisador", instanceId, termo);
             return starter.CreateCheckStatusResponse(req, instanceId);
         }
 
@@ -45,24 +44,69 @@ namespace az204function.durable
         public static async Task<string> SearchGoogle([ActivityTrigger] string termo)
         {
             return await $"https://google.com/search?=g={termo}"
-            .GetAsync()
-            .Result.GetStringAsync();
+                .GetAsync()
+                .Result.GetStringAsync();
         }
 
         [FunctionName("Bing")]
         public static async Task<string> SearchBing([ActivityTrigger] string termo)
         {
             return await $"https://bing.com/search?=g={termo}"
-            .GetAsync()
-            .Result.GetStringAsync();
+                .GetAsync()
+                .Result.GetStringAsync();
         }
 
         [FunctionName("Yahoo")]
         public static async Task<string> SearchYahoo([ActivityTrigger] string termo)
         {
             return await $"https://search.com/search?=p={termo}"
-            .GetAsync()
-            .Result.GetStringAsync();
+                .GetAsync()
+                .Result.GetStringAsync();
         }
+
+        //[FunctionName("AddFromCounter")]
+        //public static Task Run(
+        //    [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        //    [DurableClient] IDurableEntityClient client)
+        //{
+        //    var contador = req.Query["contador"];
+        //    var entityId = new EntityId(nameof(Counter), "myCounter");
+        //    var amount = int.Parse(contador);
+        //    return client.SignalEntityAsync(entityId, "Add", amount);
+        //}
+
+        //[FunctionName("CounterOrchestration")]
+        //public static async Task Run(
+        //    [OrchestrationTrigger] IDurableOrchestrationContext context)
+        //{
+        //    var entityId = new EntityId(nameof(Counter), "myCounter");
+        //    var currentValue = await context.CallEntityAsync<int>(entityId, "Get");
+        //    if (currentValue < 5)
+        //        context.SignalEntity(entityId, "Add", 1);
+        //}
+
+        //[FunctionName("Counter")]
+        //public static void Counter([EntityTrigger] IDurableEntityContext ctx)
+        //{
+        //    switch (ctx.OperationName.ToLowerInvariant())
+        //    {
+        //        case "add":
+        //            var currentValue = ctx.GetState<int>();
+        //            var amount = ctx.GetInput<int>();
+        //            if (currentValue < 100 && currentValue + amount >= 100)
+        //            {
+        //                ctx.SignalEntity(new EntityId("MonitorEntity", ""), "milestone-reached", ctx.EntityKey);
+        //            }
+
+        //            ctx.SetState(currentValue + amount);
+        //            break;
+        //        case "reset":
+        //            ctx.SetState(0);
+        //            break;
+        //        case "get":
+        //            ctx.Return(ctx.GetState<int>());
+        //            break;
+        //    }
+        //}
     }
 }
